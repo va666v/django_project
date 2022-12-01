4a4386bc9d57e89435ccd50cc75a48637d39846a
va666.v <vahek87@gmail.com>
2022-12-01 23:50:42 +0400

    New task 01.12.2022

--- mysite/tasks/urls.py
+++ mysite/tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from tasks import views
 urlpatterns = [
     path("home/", views.home_view),
-    path("welcome/", views.welcome_page)
-
+    path("intro/", views.intro_page),
+    path("datetime/", views.date_time),
+    path("task/", views.task_page)
 ]
--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -3,11 +3,20 @@ from datetime import datetime
 
 # Create your views here.
 def home_view(request):
-    print("request: ", request)
     return HttpResponse("Hello from Django")
 
 def welcome_page(request):
-    print("request: ", request)
-    return HttpResponse(f"<h1 style='text-align:center; background-color:DodgerBlue'> <b>Hello my name is Vahe !!!!! </b> </h1>"
-                        f"<br>"
-                        f"{datetime.now()}")
+    return HttpResponse("<h3 style='text-align:center; background-color:powderblue'>Hello From 1st task</h3>")
+
+def intro_page(request):
+    return HttpResponse("<h1 style='text-align:center'> <b>Python + Django </b> </h1>"
+                        "<p>Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.</p>"
+                        "<p>What can Python do? <br>1.Python can be used on a server to create web applications.<br>2.Python can be used alongside software to create workflows. <br>3.Python can connect to database systems. It can also read and modify files. <br>4.Python can be used to handle big data and perform complex mathematics. <br>5.Python can be used for rapid prototyping, or for production-ready software development.</p>")
+
+def date_time(request):
+    return HttpResponse(f"<h1 style='text-align:center; background-color:DodgerBlue'> <b>{datetime.now()}</b> </h1>")
+
+def task_page(request):
+    dect = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
+    for key, value in dect.items:
+        return HttpResponse(f"<li>{key} : {value}</li>")
